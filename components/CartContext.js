f1c4eaaba27820e8aeb362ec188aa8aba33c671f
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-11-15 07:32:02 -0500

    localstorage add

--- components/CartContext.js
+++ components/CartContext.js
@@ -5,16 +5,18 @@ export const CartContext = createContext({});
 export function CartContextProvider({children}) {
     const ls = typeof window !== "undefined" ? window.localStorage : null;
     const [cartProducts,setCartProducts] = useState([]);
+
     useEffect(() => {
     if(cartProducts?.length > 0) {
-        ls?.setItem('cart',JSON.stringify(cartProducts));
+       localStorage?.setItem('cart',JSON.stringify(cartProducts));
         }
     }, [cartProducts]);
     useEffect(() => {
-        if (ls && ls.getItem('cart')) {
-            setCartProducts(JSON.parse(ls.getItem('cart')));
+        if(localStorage && localStorage.getItem('cart')) {
+            setCartProducts(JSON.parse(localStorage.getItem('cart')));
         }
-    }, []);
+    },
+         []);
 
     function addProduct(productId){
         setCartProducts(prev => [...prev,productId]);
